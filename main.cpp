0d964be802cfa5ff2aa48313e9dde0d6ea5524dd
luisejv <luisejv@LAPTOP-J7CR1JEP.localdomain>
2020-04-10 15:43:37 -0500

    work done

--- main.cpp
+++ main.cpp
@@ -1,2 +1,26 @@
-int main (){
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+int main() {
+    int lines, countLines = 0;
+    string line, linesConsole;
+    getline(cin, linesConsole);
+    lines = stoi(linesConsole);
+    map<string, int> conquests;
+    while (countLines < lines){
+        countLines++;
+        getline(cin, line);
+        stringstream ss(line);
+        string country, conquest;
+        ss >> country;
+        conquests[country]++;
+    }
+    for (auto p : conquests){
+        cout << p.first << " " << p.second << endl;
+    }
+    return 0;
 }
